7523e5294a251ba5a830b8896bef7d739aeedf05
valentinatp <valentina.toledoporma@gmail.com>
2017-06-29 11:06:18 -0400

    Listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,25 +6,15 @@ $(document).ready(function(){
 		if (ingTarea == ""){
 			alert("Ingresa una tarea");
 		}else{
-			$("#items").append("<a href='#'><input type='checkbox' name='check' value='ok' class='work' id='com'><p id='new' class='work'>" + ingTarea + " " + "</p><i class='fa fa-trash-o elim work' aria-hidden='true'></i></a>" + "<br>");
+			$("#items").append("<a href='#'><input type='checkbox' name='check' value='ok' class='work' id='com'><p id='new' class='work'>" + ingTarea + " " + "</p><i class='fa fa-trash-o remove work' aria-hidden='true'></i></a>" + "<br>");
 			$("#tarea").val("");
 		}
 	})
 
-	$("#com").click(function(el){
-		el.preventDefault();
-		var ingTarea = $("#tarea").val();
-		$("#itemsCom").append("<a href='#'><p id='new' class='work'>" + ingTarea + " " + "</p></a>" + "<br>");
-	})
-
-	$("#items").click(function(el){
-		el.preventDefault();
-		var ingTarea = $("#tarea").val();
-		$(this).empty();
+	$(document).on('click', '.remove', function(){
+        var rem = $(this).parent().remove();
+        $("#itemsCom").append(rem);
+    });
+});
 		
 
-	})
-
-
-
-});
